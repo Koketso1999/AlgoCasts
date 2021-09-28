29240e0ad7a8ea2a1272f9342b65581cdfb944a5
Koketso Morapedi <53305777+Koketso1999@users.noreply.github.com>
2021-09-28 15:55:42 +0200

    update

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -20,7 +20,7 @@ function pyramid(n) {
     for(let row =0; row < n; row++){
 
         let level = '';
-
+ 
         for(let column = 0; column < 2 *  n-1; column++){
             if(midpoint - row <= column && midpoint + row >= column){
                 level +='#'
